3766b81bdc3cdb748741619fcd25a9fe3b013529
justin-castleman <justin-castleman@users.noreply.github.com>
2021-01-21 17:17:55 -0600

    Update deployll.sh
    Modified the GIT Clone URL to be my personal URL so that I can pull in the modified Example .ENV file.

--- deployll.sh
+++ deployll.sh
@@ -523,7 +523,8 @@ function xapi_install ()
         # TODO - make this do a max itteration of say 3 attempts to clone
         while true; do
             output_log "attempting git clone for xapi, branch: $XAPI_BRANCH"
-            git clone -q -b ${XAPI_BRANCH} https://github.com/LearningLocker/xapi-service.git ${XAPI_SUBDIR}
+            #git clone -q -b ${XAPI_BRANCH} https://github.com/LearningLocker/xapi-service.git ${XAPI_SUBDIR}
+             git clone -q -b ${XAPI_BRANCH} https://github.com/justin-castleman/xapi-service.git ${XAPI_SUBDIR}
             if [[ ! -d ${XAPI_SUBDIR} ]]; then
                 output_log "git clone appears to have failed"
                 break
